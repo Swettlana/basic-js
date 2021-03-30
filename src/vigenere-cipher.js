fdb8e27bd57b9e9993f181f7b5c3ed62a42eca97
Swettlana <stellla@tut.by>
2021-03-30 19:28:31 +0300

    fix:add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,78 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.bool = bool;
+    console.log(bool);
+  }
+  libr = "ABADEFGIJKLMNOPQRSTUVWXYZ";
+  encrypt(message, key) {
+    console.log(message, "+", key);
+    if (arguments.length !== 2) {
+      throw new Error();
+    }
+    let libr = this.libr.split("");
+    let changeToNumbers = (str) => {
+      return str
+        .toUpperCase()
+        .split("")
+        .map((char) => {
+          if (libr.includes(char)) {
+            char = libr.indexOf(char);
+          }
+        });
+    };
+    while (message.length <= key.length) {
+      key += key;
+    }
+    // заменяем символы соответсвующими цифрами
+    let arrMessage = changeToNumbers(message);
+    let arrKey = changeToNumbers(key);
+    //получаем итоговую криптограмму
+    let rezult = [],
+      curr;
+    for (let i = 0; i < arrMessage.length; i++) {
+      // if (Number.isInteger(arrMessage[i])) {
+      curr = arrMessage[i] + arrKey[i];
+      // }
+      curr > 26 ? (curr = curr - 26) : curr;
+      rezult = rezult.push(libr[curr]);
+    }
+    return this.bool ? rezult.join("") : rezult.reverse().join("");
+  }
+
+  decrypt(message, key) {
+    if (arguments.length !== 2) {
+      throw new Error();
+    }
+    let libr = this.libr.split("");
+    let changeToNumbers = (str) => {
+      return str
+        .toUpperCase()
+        .split("")
+        .map((char) => {
+          if (libr.includes(char)) {
+            char = libr.indexOf(char);
+          }
+        });
+    };
+    while (message.length <= key.length) {
+      key += key;
+    }
+    // заменяем символы соответсвующими цифрами
+    let arrMessage = changeToNumbers(message);
+    let arrKey = changeToNumbers(key);
+    //получаем итоговую криптограмму
+    let rezult = [],
+      curr;
+    for (i = 0; i < arrMessage.length; i++) {
+      if (Number.isInteger(arrMessage[i])) {
+        curr = arrMessage[i] - arrKey[i];
+      }
+      curr < 0 ? (curr = curr + 26) : curr;
+      rezult = rezult.push(libr[curr]);
+    }
+    return this.bool ? rezult.join("") : rezult.reverse().join("");
   }
 }
 

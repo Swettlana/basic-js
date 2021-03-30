ec87c22085c58ba150f78bc40850ece02b9b5bd6
Swettlana <stellla@tut.by>
2021-03-30 23:54:26 +0300

    fix:

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,55 +4,51 @@ class VigenereCipheringMachine {
   constructor(bool = true) {
     this.bool = bool;
   }
-  libr = "ABADEFGIJKLMNOPQRSTUVWXYZ";
+  libr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   encrypt(message, key) {
-    console.log(message, "+", key);
-    if (arguments.length < 2) {
-      throw new Error("");
-    }
-    // делаем строку с ключом == по длине  шифруемой  строке
-    while (key.length <= message.length) {
-      key += key;
-    }
-    //приводим к верхнему регистру строку и ключ
-    key = key.slice(0, message.length + 1).toUpperCase();
-    message = message.toUpperCase();
-    let rezult = "";
-    //клеем посимвольно результирующую строк
-    for (let i = 0; i < message.length; i++) {
-      if (!this.libr.includes(message[i])) {
-        rezult += message[i];
-      } else {
-        let codeLettersMes = this.libr.indexOf(message[i]); //числовой код каждой буквы строки
-        let codeLettersKey = this.libr.indexOf(key[i]);
-        result += libr[(codeLettersMes + codeLettersKey) % 26];
-      }
-    }
-    console.log(result);
-    return this.bool ? rezult : rezult.split("").reverse().join("");
-  }
-
-  decrypt(message, key) {
-    console.log(message, "+", key);
     if (arguments.length < 2) {
       throw new Error("");
     }
     while (key.length <= message.length) {
       key += key;
     }
-    key = key.slice(0, message.length + 1).toUpperCase();
+    key = key.toUpperCase();
     message = message.toUpperCase();
     let rezult = "";
+    let jKey = 0;
     for (let i = 0; i < message.length; i++) {
       if (!this.libr.includes(message[i])) {
         rezult += message[i];
       } else {
         let codeLettersMes = this.libr.indexOf(message[i]);
-        let codeLettersKey = this.libr.indexOf(key[i]);
-        result += libr[(codeLettersMes - codeLettersKey) % 26];
+        let codeLettersKey = this.libr.indexOf(key[jKey]);
+        rezult += this.libr[(26 + codeLettersMes + codeLettersKey) % 26];
+        jKey++;
       }
     }
-    console.log(rezult);
+    return this.bool ? rezult : rezult.split("").reverse().join("");
+  }
+  decrypt(message, key) {
+    if (arguments.length < 2) {
+      throw new Error("");
+    }
+    while (key.length <= message.length) {
+      key += key;
+    }
+    key = key.toUpperCase();
+    message = message.toUpperCase();
+    let rezult = "";
+    let jKey = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (!this.libr.includes(message[i])) {
+        rezult += message[i];
+      } else {
+        let codeLettersMes = this.libr.indexOf(message[i]);
+        let codeLettersKey = this.libr.indexOf(key[jKey]);
+        rezult += this.libr[(26 + codeLettersMes - codeLettersKey) % 26];
+        jKey++;
+      }
+    }
     return this.bool ? rezult : rezult.split("").reverse().join("");
   }
 }
